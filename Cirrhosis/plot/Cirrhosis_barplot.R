843902f81d8025f9b13118b240b18e2891148440
XiangyuYe <58210292+XiangyuYe@users.noreply.github.com>
2021-01-10 20:48:37 +0800

    Add files via upload

--- Cirrhosis/plot/Cirrhosis_barplot.R
+++ Cirrhosis/plot/Cirrhosis_barplot.R
@@ -1,6 +1,5 @@
 ########
 library(ggplot2)
-library(ggsci)
 library(patchwork)
 library(dplyr)
 rolypoly_path="/net/mulan/disk2/yasheng/test/rolypoly/single_cell_data/"
@@ -28,20 +27,21 @@ combin_output$annotation<-factor(combin_output$annotation,ordered = T,
 
 ###plot p1
 p<-ggplot(data=combin_output,aes(x=annotation,y=-log10(bp_value)))
-p<-p + geom_bar(aes(fill=Method),stat='identity',position = "dodge",width = 1,show.legend = F)+
+p<-p + geom_bar(aes(fill=Method),stat='identity',position = position_dodge(0.8),width = 0.8,show.legend = F)+
   scale_fill_manual(values=c("#DC0000B2","#4DBBD5B2"))
-p<-p + geom_hline(yintercept = -log10(0.05),color="gold",lty=5,lwd=1)
+p<-p + geom_hline(yintercept = -log10(0.025),color="gold",lty=5,lwd=1)
 p<-p + labs(x= "cell types",y = "-log10(P value)")+
   scale_y_continuous(limits=c(0,3),breaks =c(seq(0,3,0.5)) ) +
   scale_x_discrete(breaks=combin_output$annotation,
                    labels=combin_output$annotation)+coord_flip()+
-  ggtitle("(A)")
+  ggtitle("EAS")
 p1<-p+theme(panel.background=element_rect(fill = "white",color = "black"),
-             legend.text=element_text(size = 15,color = "grey20"),
-             legend.title=element_blank(),
-             panel.grid=element_blank(),
-             axis.title = element_text(size = 20,color = "black"),
-             axis.text = element_text(size = 15,color = "grey20") )
+            legend.text=element_text(size = 15,color = "grey20"),
+            legend.title=element_blank(),
+            panel.grid=element_blank(),
+            axis.title = element_text(size = 20,color = "black"),
+            axis.text = element_text(size = 18,color = "grey20"),
+            plot.title = element_text(size = 20, face = "bold",hjust = 0.5))
 
 
 ###EUR
@@ -66,21 +66,22 @@ combin_output$annotation<-factor(combin_output$annotation,ordered = T,
 
 ###plot p2
 p<-ggplot(data=combin_output,aes(x=annotation,y=-log10(bp_value)))
-p<-p + geom_bar(aes(fill=Method),stat='identity',position = "dodge",width = 1,show.legend = T)+
+p<-p + geom_bar(aes(fill=Method),stat='identity',position = position_dodge(0.8),width = 0.8,show.legend = T)+
   scale_fill_manual(values=c("#DC0000B2","#4DBBD5B2"))
-p<-p + geom_hline(yintercept = -log10(0.05),color="gold",lty=5,lwd=1)
+p<-p + geom_hline(yintercept = -log10(0.025),color="gold",lty=5,lwd=1)
 p<-p + labs(x= "cell types",y = "-log10(P value)")+
   scale_y_continuous(limits=c(0,3),breaks =c(seq(0,3,0.5)) ) +
   scale_x_discrete(breaks=combin_output$annotation,
                    labels=combin_output$annotation)+coord_flip()+
-  ggtitle("(B)")
+  ggtitle("EUR")
 p2<-p+theme(panel.background=element_rect(fill = "white",color = "black"),
-                       legend.text=element_text(size = 15,color = "grey20"),
-                       legend.title=element_blank(),
-                       panel.grid=element_blank(),
-                       axis.title.y=element_blank(),
-                       axis.title = element_text(size = 20,color = "black"),
-                       axis.text = element_text(size = 15,color = "grey20") )
+            legend.text=element_text(size = 15,color = "grey20"),
+            legend.title=element_blank(),
+            panel.grid=element_blank(),
+            axis.title.y=element_blank(),
+            axis.title = element_text(size = 20,color = "black"),
+            axis.text = element_text(size = 18,color = "grey20") ,
+            plot.title = element_text(size = 20,face = "bold",hjust = 0.5))
 
 ###plot out
 pdf(paste0("/net/mulan/disk2/yasheng/test/rolypoly/single_cell_data/GSE136103_cirrhosis/estimates_cir.pdf"),height = 9,width = 12)
